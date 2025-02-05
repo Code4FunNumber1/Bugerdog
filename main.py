4379547a246dfbed0b3b6802e569fc1ef8088990
Jacob A. Stephens <stephjac000@warren.k12.in.us>
2025-02-05 09:47:04 -0500

    Easy

---
+++ main.py
@@ -0,0 +1,46 @@
+import random, pygame
+
+pygame.init()
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Burger Dog")
+
+FPS = 60
+clock = pygame.time.Clock()
+
+PLAYER_STARTING_LIVES = 3
+PLAYER_NORMAL_VELOCITY = 5
+PLAYER_BOOST_VELOCITY = 10
+STARTING_BOOST_LEVEL = 100
+STARTING_BURGER_VELOCITY = 3
+BURGER_ACCELERATION = 0.5
+BUFFER_DISTANCE = 100
+
+score = 0
+burger_points = 0
+burgers_eaten = 0
+
+player_lives = PLAYER_STARTING_LIVES
+player_velocity = PLAYER_NORMAL_VELOCITY
+boost_level = STARTING_BOOST_LEVEL
+burger_velocity = STARTING_BURGER_VELOCITY
+
+ORANGE = (246, 124, 54)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+font = pygame.font.Font("WashYourHand.ttf", 32)
+
+
+
+def prep_text(text: str, background_color: tuple[int, int, int], **locations):
+    text_to_return = font.render(text, True, background_color)
+    rect = text_to_return.get_rect()
+    for location in locations:
+        if location == "topleft":
+            rect.topleft = locations["topleft"]
+        elif location == "centerx":
+            rect.centerx = locations["centerx"]
+    return (text_to_return, rect)
